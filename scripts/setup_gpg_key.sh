82cb35ef6a238b51350951ef130d825dff426f00
Ewan Harris <ewan.harris@okta.com>
2024-01-26 17:59:46 +0000

    chore(ci): replace usage of secrets and scripts with actions usage

--- scripts/setup_gpg_key.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-if [ -z "$GPG_SECRET_KEY" ]; then
-  echo "GPG Key not found - skipping"
-  exit 0
-fi
-
-echo "Importing GPG Key"
-echo -n "$GPG_SECRET_KEY" | base64 --decode | gpg --import
